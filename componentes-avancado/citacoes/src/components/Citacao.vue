07fccdcda91155f3dab7e53e54a4c045b1ca244e
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-12-06 08:06:32 -0300

    criando repositório, adicionando projetos mais recentes

---
+++ componentes-avancado/citacoes/src/components/Citacao.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot></slot>
+        <div class="autor"><slot name="fonte"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    // created() {
+    //     console.log('created')
+    // },
+    // destroyed() {
+    //     console.log('destroyed')
+    // },
+    // activated() {
+    //     console.log('activated')
+    // },
+    // deactivated() {
+    //     console.log('desactivated')
+    // }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    .autor {
+        float: right;
+    }
+</style>
---
+++ components/componente-desafio/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-app>
+		<menu-vue />
+
+		<navbar-vue />
+
+		<carrosel-vue />
+
+		<footer-vue />
+	</v-app>
+</template>
+
+<script>
+import Carrosel from '@/components/widgets/Carrosel.vue'
+import Footer from '@/components/template/Footer.vue'
+import NavBar from '@/components/template/NavBar.vue'
+import Menu from '@/components/template/Menu.vue'
+
+export default {
+	components: { 'menu-vue': Menu, 'navbar-vue': NavBar,
+		'carrosel-vue': Carrosel, 'footer-vue': Footer }
+}
+</script>
